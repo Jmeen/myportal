b09cd1e185e2b9ded5453a2abfb5f7bc31e24ae5
Jaemin.Lim <jmeeen@gmail.com>
2021-09-29 13:54:20 +0900

    ViewResolver 추가

--- myportal/src/main/java/com/bitacademy/myportal/controller/myhomeController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controller/myhomeController.java
@@ -10,7 +10,8 @@ public class myhomeController {
 	public ModelAndView home () {
 		ModelAndView mav = new ModelAndView();
 		// 뷰 파일을 연결 : 서블릿에서 RequestDispatcher로 포워드 한 것과 비슷
-		mav.setViewName("/WEB-INF/views/home.jsp");
+//		mav.setViewName("/WEB-INF/views/home.jsp");
+		mav.setViewName("home"); // viewResolver가 prefix와 suffix 조합하여 경로 생성
 		return mav;
 	}
 	
